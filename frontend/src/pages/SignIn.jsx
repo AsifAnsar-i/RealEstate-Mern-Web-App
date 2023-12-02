681e7cadd6147f8d42dab4d273c79297fa2d5929
AsifAnsar-i <91591849+AsifAnsar-i@users.noreply.github.com>
2023-12-03 02:48:54 +0530

    login UI

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,9 +1,27 @@
-import React from "react";
-
+import React, { useState } from "react";
+import {Link} from "react-router-dom"
 const SignIn = () => {
+  const [email,setEmail] =useState("")
+  const [password,setPassword] =useState("")
+
+  const onSubmit=(e)=>{
+    e.preventDefault();
+    console.warn({email,password})
+  }
   return (
-    <div>
-      <>SignIn</>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">SignIn</h1>
+      <form onSubmit={onSubmit} className="flex flex-col gap-4 ">
+        <input type="email" placeholder="email" className="border p-3 rounded-lg focus:outline-none" id="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+        <input type="password" placeholder="password" className="border p-3 rounded-lg focus:outline-none" id="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Sign up</button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Don't have an account?</p>
+        <Link to="/sign-up">
+          <span className="text-blue-700">Sign in</span>
+        </Link>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import {Link} from "react-router-dom"
 const SignUp = () => {
+  const [username,setUsername] =useState("")
+  const [email,setEmail] =useState("")
+  const [password,setPassword] =useState("")
+
+  const onSubmit=(e)=>{
+    e.preventDefault();
+    console.warn({username,email,password})
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">SignUp</h1>
-      <form className="flex flex-col gap-4 ">
-        <input type="text" placeholder="username" className="border p-3 rounded-lg" id="username" />
-        <input type="email" placeholder="email" className="border p-3 rounded-lg" id="email" />
-        <input type="password" placeholder="password" className="border p-3 rounded-lg" id="password" />
+      <form onSubmit={onSubmit} className="flex flex-col gap-4">
+        <input type="text" placeholder="username" className="border p-3 rounded-lg focus:outline-none" id="username" value={username} onChange={(e)=>setUsername(e.target.value)} />
+        <input type="email" placeholder="email" className="border p-3 rounded-lg focus:outline-none" id="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
+        <input type="password" placeholder="password" className="border p-3 rounded-lg focus:outline-none" id="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
         <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Sign up</button>
       </form>
       <div className="flex gap-2 mt-5">
