501d7a440b5109f647e515c298fc956cd88301e4
AsifAnsar-i <91591849+AsifAnsar-i@users.noreply.github.com>
2023-12-03 01:00:58 +0530

    register routes and controllers

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import cors from "cors";
+import morgan from "morgan";
 
 dotenv.config();
 
@@ -9,7 +12,13 @@ connectDB();
 
 const app = express();
 
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
 const port = 8080;
+
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(port, () => {
   console.warn(`Server running on ${port}`.bgMagenta.white);
