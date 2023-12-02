5774a3e4728eb16b48f67fa32b46e036881dcc10
AsifAnsar-i <91591849+AsifAnsar-i@users.noreply.github.com>
2023-12-02 23:53:26 +0530

    realEstate web app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div className="text-red-600 text-4xl text-center">App</div>
+    </div>
+  );
+};
+
+export default App;
