72c1a278a78fd8319ca3e261c87411abfad17aa1
AsifAnsar-i <91591849+AsifAnsar-i@users.noreply.github.com>
2023-12-03 00:55:14 +0530

    userModel

---
+++ models/userModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    role: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+export default mongoose.model("User", userSchema);
